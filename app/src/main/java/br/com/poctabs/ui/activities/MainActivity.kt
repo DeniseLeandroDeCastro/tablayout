62636af66191cbf14fe7d66fd1295ea5afaedc03
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2023-01-30 21:03:12 -0300

    Adicionando Fragments e novas cores

--- app/src/main/java/br/com/poctabs/ui/activities/MainActivity.kt
+++ app/src/main/java/br/com/poctabs/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.poctabs
+package br.com.poctabs.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.poctabs.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
