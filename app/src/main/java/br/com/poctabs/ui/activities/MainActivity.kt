abf0232b4d327b807b5f43052443b4dd577956f3
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2023-01-30 22:09:37 -0300

    Vinculando as views utilizando o view binding na MainActivity

--- app/src/main/java/br/com/poctabs/ui/activities/MainActivity.kt
+++ app/src/main/java/br/com/poctabs/ui/activities/MainActivity.kt
@@ -3,10 +3,28 @@ package br.com.poctabs.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.poctabs.R
+import br.com.poctabs.databinding.ActivityMainBinding
+import br.com.poctabs.ui.adapter.ViewPagerAdapter
+import br.com.poctabs.ui.fragments.TabOneFragment
+import br.com.poctabs.ui.fragments.TabThreeFragment
+import br.com.poctabs.ui.fragments.TabTwoFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter=ViewPagerAdapter(supportFragmentManager)
+
+        adapter.addFragment(TabOneFragment(), "Tab 1")
+        adapter.addFragment(TabTwoFragment(), "Tab 2")
+        adapter.addFragment(TabThreeFragment(), "Tab 3")
+
+        binding.viewPager.adapter=adapter
+        binding.tbLayout.setupWithViewPager(binding.viewPager)
     }
 }
